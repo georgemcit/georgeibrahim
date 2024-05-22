6717d2288fb0623f5b3f269f4b3ec2564d1f7893
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-22 18:47:22 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,30 +1,31 @@
-variable "prefix" {
-  default = "tfvmex"
+/*
+locals{
+   vm_list=["firstvm","secondvm","thirdvm","fourthvm","fifthvm"]
 }
-
-resource "azurerm_resource_group" "example" {
+*/
+resource "azurerm_resource_group" "george" {
   name     = "${var.prefix}-resources"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "ibrahim" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.george.location
+  resource_group_name = azurerm_resource_group.george.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.george.name
+  virtual_network_name = azurerm_virtual_network.ibrahim.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "ibrahim" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.george.location
+  resource_group_name = azurerm_resource_group.george.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -33,7 +34,7 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_virtual_machine" "ibrahim" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
@@ -69,4 +70,7 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     environment = "staging"
   }
+}
+variable "prefix" {
+  default = "tfvmex"
 }
