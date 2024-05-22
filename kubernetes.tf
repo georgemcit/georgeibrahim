e5eac45899cb887068821a189b068e174d71b7a7
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-21 23:34:42 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -101,15 +101,20 @@ default_node_pool {
   }
 }
 */
-/*
-locals{
-   cluster_list=["montreal","toronto","vancouver","alberta"]
+locals{ 
+  cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
 }
-resource "azurerm_kubernetes_cluster" "georgeibrahimcluster" {
-  name                = "georgeibrahimcluster"
-  location            = azurerm_resource_group.georgeibrahim.location
-  resource_group_name = azurerm_resource_group.georgeibrahim.name
-  dns_prefix          = "ccrf2301"
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "MCIT_resource_group"
+  location = "Canada Central"
+}
+
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in var.classworkclusters: cluster=>cluster}
+  name                = "${var.prefix}${each.key}"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = var.dnsprefix
 
   default_node_pool {
     name       = "default"
@@ -125,92 +130,20 @@ resource "azurerm_kubernetes_cluster" "georgeibrahimcluster" {
     Environment = "Production"
   }
 }
-resource "azurerm_kubernetes_cluster" "george" {
-  for_each            = {for cluster in local.cluster_list: cluster=>cluster}
-  name                = "${var.prefix}cluster"
-  location            = azurerm_resource_group.georgeibrahim.location
-  resource_group_name = azurerm_resource_group.georgeibrahim.name
-  dns_prefix          = "ccrf2301"
+output "client_certificate" {
+  value     = [for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.kube_config.0.client_certificate]
+  sensitive = true
+}
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
+output "kube_config" {
+  value = [for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config_raw]
 
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
+  sensitive = true
+}
+output "kube_id"{
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.id ]
 }
 output "kube_name"{
-  value=[for cluster in azurerm_kubernetes_cluster.george:cluster.name ]
-}
-variable "identity"{
- type=string
- default="SystemAssigned"
-}
-output "identity" {
-  value = var.identity
-}
-variable "tags"{
- type=string
- default= "Production"
-}
-output "tags" {
-  value = var.tags
-}
-resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
-for_each               = azurerm_kubernetes_cluster.george
-name                   = "${each.key}"
-kubernetes_cluster_id  = each.value.id
-vm_size                = "Standard_DS2_v2"
-node_count             = 1 
-  tags = {
-    Environment = "Production"
-  }
-}
-output "id" {
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.id
-  ]
-}
- 
-output "kube_config" {
-  sensitive = true
-  value = [ 
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config_raw
-  ]
-}
- 
-output "client_key" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.client_key
-  ]
-}
- 
-output "client_certificate" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.client_certificate
-  ]
-}
- 
-output "cluster_ca_certificate" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.cluster_ca_certificate
-  ]
-}
- 
-output "host" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.host
-  ]
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.name ]
 }
 */
