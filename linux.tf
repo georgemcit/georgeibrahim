ed5c410c7c00ac8bd282c12d8096779d16f7f8f7
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-21 22:55:14 -0400

    Create linux.tf

---
+++ linux.tf
@@ -0,0 +1,29 @@
+locals{
+  linux_app=[for f in fileset("${path.module}/${var.linuxappconfiguration}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.linuxappconfiguration}/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        name=linuxapps.name
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name  
+      }
+    ]
+])
+}
+resource "azurerm_service_plan" "george" {
+  for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.george_ibrahim.name
+  location            = azurerm_resource_group.george_ibrahim.location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
+}
+
+resource "azurerm_linux_web_app" "george1980" {
+  for_each            = azurerm_service_plan.george
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.george_ibrahim.name
+  location            = azurerm_resource_group.george_ibrahim.location
+  service_plan_id     = each.value.id
+ site_config {}
+}
