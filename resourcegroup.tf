dc1f0b46d252d25c9508cc284d044f6647c2e2c3
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-22 19:58:01 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,10 +1,9 @@
 locals{
-  rg_app=[for f in fileset("${path.module}/resource, "[^_]*.yaml") : yamldecode(file("${path.module}/resource/${f}"))]
-  rg_app_list = flatten([
-    for app in local.rg_app : [
-      for rgapps in try(app.resourcegroup, []) :{
-        name= rgapps.name
-        location =rgapps.location  
+  resource_group=[for f in fileset("${path.module}/resource", "[^_]*.yaml") : yamldecode(file("${path.module}/resource/${f}"))]
+  resourcegroup_list = flatten([
+    for app in local.resource_group: [
+      for resourcegroup in try(app.listofresourcegroup, []) :{
+        name=resourcegroup.name
       }
     ]
 ])
