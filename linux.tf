0317c39d54027b18959175aea2810d827a7e8999
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-21 22:56:48 -0400

    Update linux.tf

--- linux.tf
+++ linux.tf
@@ -13,8 +13,8 @@ locals{
 resource "azurerm_service_plan" "george" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.george_ibrahim.name
-  location            = azurerm_resource_group.george_ibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+  location            = azurerm_resource_group.georgeibrahim.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -22,8 +22,31 @@ resource "azurerm_service_plan" "george" {
 resource "azurerm_linux_web_app" "george1980" {
   for_each            = azurerm_service_plan.george
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.george_ibrahim.name
-  location            = azurerm_resource_group.george_ibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+  location            = azurerm_resource_group.georgeibrahim.location
   service_plan_id     = each.value.id
  site_config {}
+}
+variable "environment"{
+ type=string
+ default="staging"
+}
+variable "linuxappconfiguration"{
+ type=string
+ default="linuxappconfiguration"
+}
+output "azure_linux_app" {
+  value = local.linux_app_list
+}
+output "names_linux_app" {
+  value=[for app in local.linux_app_list : app.name]
+}
+output "os_linux_app" {
+  value=[for app in local.linux_app_list : app.os_type]
+}
+output "sku_linux_app" {
+  value=[for app in local.linux_app_list : app.sku_name]
+}
+output "linuxappconfiguration" {
+  value = var.linuxappconfiguration
 }
