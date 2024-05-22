bdd478e2635c5b44b40b045e5b8ce6c4b9ed4dbb
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-22 19:27:31 -0400

    Update test1.tf

--- test1.tf
+++ test1.tf
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "ibrahim" {
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.george.name
-  virtual_network_name = "${each.key}"
+  virtual_network_name = azurerm_virtual_network.ibrahim.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
