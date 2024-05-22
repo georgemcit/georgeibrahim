a0803c816b224ac10fbd8a4d4e4e5340dffa7b50
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-22 19:43:13 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,6 +1,11 @@
-/*
-resource "azurerm_resource_group" "georgeibrahim" {
-  name     = "george_ibrahim"
-  location = "Canada Central"
+locals{
+  rg_app=[for f in fileset("${path.module}/resource, "[^_]*.yaml") : yamldecode(file("${path.module}/resource/${f}"))]
+  rg_app_list = flatten([
+    for app in local.rg_app : [
+      for rgapps in try(app.resourcegroup, []) :{
+        name=george_ibrahim
+        location: Canada Central 
+      }
+    ]
+])
 }
-*/
