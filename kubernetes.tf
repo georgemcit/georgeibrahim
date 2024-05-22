98f1d135226b25c4d680e268ce067fa789bd04d9
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-21 22:25:33 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -126,7 +126,7 @@ resource "azurerm_kubernetes_cluster" "georgeibrahimcluster" {
 }
 resource "azurerm_kubernetes_cluster" "george" {
   for_each            = {for cluster in local.cluster_list: cluster=>cluster}
-  name                = "${var.prefix}${each.key}"
+  name                = "${var.prefix}cluster"
   location            = azurerm_resource_group.georgeibrahim.location
   resource_group_name = azurerm_resource_group.georgeibrahim.name
   dns_prefix          = "ccrf2301"
