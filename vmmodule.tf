b11680beab9144a3d34ee9719aefd00f43dbc8b9
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-28 21:46:46 -0400

    Update vmmodule.tf

--- vmmodule.tf
+++ vmmodule.tf
@@ -5,17 +5,3 @@ module "module_win_vms" {
   admin_username      = var.admin_username  
   admin_password      = var.admin_password 
 }
-variable "admin_username"{
-  type=string
-}
-output "admin_username" {
-  sensitive = true
-  value = var.admin_username
-}
-variable "admin_password"{
-  type=string
-}
-output "admin_password" {
-  sensitive = true
-  value = var.admin_password 
-}
