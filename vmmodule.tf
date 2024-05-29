d9180dab970493163849eaa06c7386f5d374c6c5
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-28 22:39:27 -0400

    Update vmmodule.tf

--- vmmodule.tf
+++ vmmodule.tf
@@ -1,9 +1,11 @@
 module "module_win_vms" {
   source              = "./modulewinvms/"
   resource_group_name = "george_ibrahim"
-  location            = "West Europe"
-  admin_username      = var.admin_username  
-  admin_password      = var.admin_password 
+  location                     = "West Europe"
+  admin_username               = var.admin_username  
+  admin_password               = var.admin_password 
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
 }
 variable "admin_username"{
   type=string
@@ -18,4 +20,18 @@ variable "admin_password"{
 output "admin_password" {
   sensitive = true
   value = var.admin_password 
+}
+variable "administrator_login"{
+  type=string
+}
+output "administrator_login" {
+  sensitive = true
+  value = var.administrator_login
+}
+variable "administrator_login_password"{
+  type=string
+}
+output "administrator_login_password" {
+  sensitive = true
+  value = var.administrator_login_password 
 }
