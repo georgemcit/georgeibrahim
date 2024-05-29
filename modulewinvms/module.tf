451dfb6531bc598832a106f976ca082b0a5e1c51
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-28 21:43:32 -0400

    Update module.tf

--- modulewinvms/module.tf
+++ modulewinvms/module.tf
@@ -3,41 +3,41 @@ resource "azurerm_resource_group" "georgeibrahim" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "georgevn" {
+  name                = "george-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.georgeibrahim.location
   resource_group_name = azurerm_resource_group.georgeibrahim.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
+resource "azurerm_subnet" "georgesb" {
+  name                 = "george_sb"
   resource_group_name  = azurerm_resource_group.georgeibrahim.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+resource "azurerm_network_interface" "georgenic" {
+  name                = "george-nic"
   location            = azurerm_resource_group.georgeibrahim.location
   resource_group_name = azurerm_resource_group.georgeibrahim.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.georgesb.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
+resource "azurerm_windows_virtual_machine" "georgevm" {
+  name                = "george-machine"
   resource_group_name = azurerm_resource_group.georgeibrahim.name
   location            = azurerm_resource_group.georgeibrahim.location
   size                = "Standard_F2"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.georgenic.id,
   ]
 
   os_disk {
