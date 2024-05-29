fffa29f65fa8245d147b78dfa7cb9931ab1a25de
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-28 22:36:47 -0400

    Update module.tf

--- modulewinvms/module.tf
+++ modulewinvms/module.tf
@@ -74,6 +74,24 @@ resource "azurerm_windows_virtual_machine" "georgevm2" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
+
+resource "azurerm_mssql_server" "georgesql" {
+  name                         = "georgemssqlserver"
+  resource_group_name          = azurerm_resource_group.georgeibrahim.name
+  location                     = azurerm_resource_group.georgeibrahim.location
+  version                      = "12.0"
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
 }
 resource "azurerm_web_application_firewall_policy" "georgewaf" {
   name                = "george-wafpolicy"
