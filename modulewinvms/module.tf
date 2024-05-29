7a15885ea51a2985b84f75ec0f3d46e6c4cff7e7
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-28 21:38:18 -0400

    Update module.tf

--- modulewinvms/module.tf
+++ modulewinvms/module.tf
@@ -9,21 +9,21 @@ resource "azurerm_resource_group" "georgeibrahim" {
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.georgeibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = azurerm_resource_group.georgeibrahim.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.georgeibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
 
   ip_configuration {
     name                          = "internal"
@@ -34,8 +34,8 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+  location            = azurerm_resource_group.georgeibrahim.location
   size                = "Standard_F2"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
