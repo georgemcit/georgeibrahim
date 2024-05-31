527da9736e9362e7c12c5794d7da38d08d236a6c
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-31 18:01:25 -0400

    Create localmodule.tf

---
+++ localmodule.tf
@@ -0,0 +1,19 @@
+/*
+module "module_vnet" {
+  source              = "./vnet/"
+  resource_group_name = local.resource_group_name
+  location = local.location
+  vnet_name = local.vnet_name
+  address_space = local.address_space
+  subnet_name = local.subnet_name
+  address_prefixes = local.address_prefixes
+}
+locals{
+  resource_group_name="georgeibrahim"
+  location="Canada Central"
+  vnet_name="georgevnet"
+  address_space=["10.0.0.0/16"]
+  subnet_name="georgesb"
+  address_prefixes=["10.0.1.0/24"]
+}
+*/
